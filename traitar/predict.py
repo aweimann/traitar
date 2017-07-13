aa246366d478d53ba4fe30a578f5ca02b7ef4a10
Aaron Weimann <aaron.weimann@uni-duesseldorf.de>
2017-07-13 21:51:43 +0200

    predicty.py modified
    - added some code to include feature standardization info in pt models

--- traitar/predict.py
+++ traitar/predict.py
@@ -1,5 +1,5 @@
 import os.path
-import pandas as ps
+import pandas as pd
 import sys
 from .PhenotypeCollection import PhenotypeCollection
 
@@ -11,17 +11,17 @@ def filter_pred(scores, is_majority, k):
         if (scores > 0).sum() >= (k/2 + 1):
             return scores[scores >= 0].mean()
         else:
-            return ps.np.NaN
+            return pd.np.NaN
     else:
         if (scores > 0).all():
             return scores.mean()
         else:
-            return ps.np.NaN
+            return pd.np.NaN
 
 def aggregate(pred_df, k, out_dir, pt2acc):
     """employ different prediction strategies"""
     out = ["majority-vote", "conservative-vote", "single-votes"]
-    maj_pred_dfs = ps.DataFrame(ps.np.zeros(shape = (pred_df.shape[0], pred_df.shape[1] / k)), columns = ["%s" % i for i in range(pred_df.shape[1]/k)])
+    maj_pred_dfs = pd.DataFrame(pd.np.zeros(shape = (pred_df.shape[0], pred_df.shape[1] / k)), columns = ["%s" % i for i in range(pred_df.shape[1]/k)])
     maj_pred_dfs.index = pred_df.index
     maj_pred_dfs_columns = maj_pred_dfs.columns.tolist()
     for i in range(pred_df.shape[1] / k):
@@ -40,37 +40,47 @@ def majority_predict(pt, pt_models, test_data, k, bias_weight = 1):
     """predict the class label based on a committee vote of the models in models""" 
     #TODO if the classification model is trained on non binary data we would require the same normalization applied to the training data 
     #binarize
-    test_data_n = (test_data > 0).astype('int')
+    if not pt_models.is_standardized:
+        test_data_n = (test_data > 0).astype('int')
     #check if classification model exists for the requested phenotype
     try: 
         bias = pt_models.get_bias(pt)
         predictors = pt_models.get_predictors(pt)
     except KeyError:
-        return ps.DataFrame()
+        return pd.DataFrame()
     #build prediction matrix with the k best models
-    preds = ps.np.zeros((test_data.shape[0], k))
+    preds = pd.np.zeros((test_data.shape[0], k))
     for i in range(k):
+        if pt_models.is_standardized:
+            scale, mean = pt_models.get_scale_and_mean(pt)
+            from sklearn import preprocessing
+            scaler = preprocessing.StandardScaler()
+            scaler.mean_ = mean.T
+            scaler.scale_ = scale.T
+            test_data_n = pd.DataFrame(scaler.transform(test_data))
+            test_data_n.index = test_data.index
+            test_data_n.columns = test_data.columns
         preds[:, i] = bias.iloc[i, 0] * bias_weight +  predictors.iloc[:, i].dot(test_data_n.loc[:, predictors.iloc[:, i].index].T)
-        pred_df = ps.DataFrame(preds, index = test_data.index)
+        pred_df = pd.DataFrame(preds, index = test_data.index)
     #set column names
     pred_df.columns = ["%s_%s" %(pt, predictors.columns[i].split("_")[0]) for i in range(k)]
     return pred_df
 
 def annotate_and_predict(pt_models, summary_f, out_dir, k):
     """load annotation previously generated with HMMER and predict phenotypes with phypat and phypat+GGL models"""
-    pred_df = ps.DataFrame()
+    pred_df = pd.DataFrame()
     #read pfam to description file
     pfam_mapping = pt_models.get_pf2desc()
     pt_mapping = pt_models.get_pt2acc()
     #read annotation file
-    m = ps.read_csv(summary_f, sep="\t", index_col = 0)
+    m = pd.read_csv(summary_f, sep="\t", index_col = 0)
     #restrict to those pfams contained in the model
     #m_red = m.loc[:, pfam_mapping.index ].astype('int')
     #m.columns = pfam_mapping.index
     for pt in pt_mapping.index:
         #predict an append to prediction df
         preds = majority_predict(pt, pt_models, m, k)
-        pred_df = ps.concat([pred_df, preds], axis = 1)
+        pred_df = pd.concat([pred_df, preds], axis = 1)
     pred_df.index = m.index
     pred_df.to_csv("%s/predictions_raw.txt"%out_dir, sep = "\t", float_format='%.3f')
     #aggregate predictions
